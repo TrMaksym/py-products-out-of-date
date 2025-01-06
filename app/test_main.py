ccac0c2c26e24195f096afc946d8660ea4c5aa0d
unknown <maximuschampion2002@gmail.com>
2025-01-07 00:53:28 +0200

    products-out-of-date

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,43 @@
-# write your code here
+import pytest
+from unittest import mock
+import datetime
+from app.main import outdated_products
+
+@pytest.mark.parametrize(
+    "products, today_date, expected_outdated",
+    [
+        (
+            [
+                {"name": "salmon", "expiration_date": datetime.date(2022, 2, 10), "price": 600},
+                {"name": "chicken", "expiration_date": datetime.date(2022, 2, 5), "price": 120},
+                {"name": "duck", "expiration_date": datetime.date(2022, 2, 1), "price": 160}
+            ],
+            datetime.date(2022, 2, 6),
+            ["chicken", "duck"]
+        ),
+        (
+            [
+                {"name": "salmon", "expiration_date": datetime.date(2022, 2, 10), "price": 600},
+                {"name": "chicken", "expiration_date": datetime.date(2022, 2, 5), "price": 120},
+                {"name": "duck", "expiration_date": datetime.date(2022, 2, 1), "price": 160}
+            ],
+            datetime.date(2022, 2, 1),
+            []
+        ),
+        (
+            [
+                {"name": "salmon", "expiration_date": datetime.date(2022, 2, 10), "price": 600},
+                {"name": "chicken", "expiration_date": datetime.date(2022, 2, 5), "price": 120},
+                {"name": "duck", "expiration_date": datetime.date(2022, 2, 1), "price": 160}
+            ],
+            datetime.date(2022, 2, 6),
+            ["chicken", "duck"]
+        )
+    ]
+)
+def test_outdated_products(products, today_date, expected_outdated):
+    with mock.patch("app.main.datetime.date") as mock_date:
+        mock_date.today.return_value = today_date
+        result = outdated_products(products)
+        print(f"Result for {today_date}: {result}")
+        assert result == expected_outdated
